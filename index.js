55aa8a518d4e6ac7fcac1eee0c358e6ed91bc2aa
hmzsjd <github@hamzasajid.com>
2023-02-05 13:52:51 +0300

    .

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let ttBarBuy = document.getElementById("ttBuy");
 
 async function goldPrice() {
   let resp = await axios.get(
-    "https://marketdata.tradermade.com/api/v1/live?currency=XAUUSD&api_key=wLD-WysSDKBfHki3b3K9"
+    "https://marketdata.tradermade.com/api/v1/live?currency=XAUUSD&api_key=gUkkarv9QS3rDdC4nbNk"
   );
   console.log(resp.data.quotes[0].ask);
   return resp.data.quotes[0].ask;
